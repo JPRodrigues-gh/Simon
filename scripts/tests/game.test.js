573d7a907a3724bd3dac0427e43d898327763fd5
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-06-22 03:56:55 +0000

    Add newGame and showScore functions and tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,8 +1,8 @@
 /**
  * @jest-environment jsdom
  */
-// const { test } = require("picomatch");
-const { game } = require("../game");
+
+const { game, newGame, showScore } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -24,5 +24,34 @@ describe("game object contains correct keys", () => {
     });
     test("gameChoices key exists", () => {
         expect("gameChoices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.gameChoices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.currentGame = ["button1", "button2"];
+        game.playerMoves = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear current game array", () => {
+        expect(game.currentGame.length).toBe(0);
+        //model solution
+        // expect(game.currentGame).toEqual([]);
+    });
+    test("should clear player moves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+        //model solution
+        // expect(game.playerMoves).toEqual([]);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
