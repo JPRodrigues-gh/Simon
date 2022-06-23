49d4c80258c179ad7583a512805e9ce3a3228e02
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-06-23 23:12:43 +0000

    Add code and test for an aleret if player makes a wrong move

--- scripts/game.js
+++ scripts/game.js
@@ -57,11 +57,26 @@ function showTurns() {
     }, 800);
 }
 
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length == game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        } 
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
+}
+
 module.exports = {
     game,
     newGame,
     showScore,
     addTurn,
     lightsOn,
-    showTurns
+    showTurns,
+    playerTurn
 };
